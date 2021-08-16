c8c86d8136ba190d0636afa55f80fa689e94302b
klavrenuk <klavrenuk@yandex.ru>
2021-08-16 18:25:59 +0300

    create and connect dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auth from './components/auth/Auth';
+import Dashboard from "./components/dashboard/Dashboard";
 
 
 const App = () => {
     return (
-        <Auth />
+        <Dashboard />
     )
 }
 
