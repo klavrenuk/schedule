e673fe0bfcc1f6256a8005b5b9a5de9623792429
klavrenuk <klavrenuk@yandex.ru>
2021-08-19 07:49:01 +0300

    NavigationDays - html coding

--- src/components/dashboard/NavigationDays.jsx
+++ src/components/dashboard/NavigationDays.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Button} from 'reactstrap';
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
 
 const NavigationDays = () => {
     return (
         <div className={'navigation_days'}>
-            Today
+            <Button
+                color={'link'}
+                className={'navigation_days-arrow navigation_days-arrow--prev'}
+            >
+                <BsChevronLeft />
+            </Button>
+            <Button color={'link'}
+                    className={'navigation_days-today'}
+            >Today</Button>
+            <Button
+                color={'link'}
+                className={'navigation_days-arrow navigation_days-arrow-next'}
+            >
+                <BsChevronRight />
+            </Button>
         </div>
     )
 }
