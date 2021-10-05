0bbfdbda715e311d9187abc4734b86acdb6cfe94
klavrenuk <klavrenuk@yandex.ru>
2021-10-05 22:09:43 +0300

    Demo - create page

---
+++ src/components/demo/Demo.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Button, Container} from 'reactstrap';
+
+import './demo.min.css';
+
+const buttons = [
+    'primary',
+    'secondary',
+    'success',
+    'info',
+    'warning',
+    'danger',
+    'link',
+    'icon'
+];
+
+export default function Demo() {
+    return (
+        <Container fluid={true}>
+            <section className={'demo'}>
+                <h1>Demo</h1>
+
+                <article>
+                    {
+                        buttons.map((button) => {
+                            return <Button color={button}>{button}</Button>
+                        })
+                    }
+                </article>
+            </section>
+        </Container>
+    )
+}
