40a3a935a76b8c927c1302ff7a180da6ec8a51aa
klavrenuk <klavrenuk@yandex.ru>
2021-10-05 22:50:17 +0300

    EventForm - Add call component

--- src/components/aside/Aside.jsx
+++ src/components/aside/Aside.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {Button} from 'reactstrap';
+
+import ModalWithComponent from "../general/ModalWithComponent";
+import EventForm from "../event/EventForm";
 
 import './aside.min.css';
 
 const Aside = () => {
+    const ModalWithComponentRef = useRef();
+
+    const openModalCreateEvent = () => ModalWithComponentRef.current.show();
+
     return (
-        <aside className={'aside'}>Aside</aside>
+        <aside className={'aside'}>
+            <Button color={'white'}
+                    block={true}
+                    onClick={openModalCreateEvent}
+            >Create event</Button>
+
+            <ModalWithComponent ref={ModalWithComponentRef}
+                                title={'Create EventForm'}
+                                component={EventForm}
+            />
+        </aside>
     )
 }
 
---
+++ src/components/event/EventForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function EventForm() {
+    return (
+        <div>Hello from Event</div>
+    )
+}
