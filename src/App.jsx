169d6fe5e1850de8e692e4816b54265edf0a1c07
klavrenuk <klavrenuk@yandex.ru>
2022-03-19 21:50:26 +0300

    UI: method who-i-am

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 import Demo from './components/demo/Demo';
 import Auth from './components/auth/Auth';
@@ -10,21 +11,63 @@ import {
     Route,
     Redirect,
 } from "react-router-dom";
+import Loading from "./components/general/Loading";
 
 const App = () => {
-    const [isAuthorized, setIsAuthorized] = useState(true);
+    const [isAuthorized, setIsAuthorized] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isQuery, setIsQuery] = useState(true);
+
+    useEffect(() => {
+        if(isQuery && location.pathname !== '/') {
+            whoIAM();
+            setIsQuery(false);
+
+        } else {
+            setIsLoading(false);
+        }
+
+    }, [isQuery]);
+
+    const whoIAM = () => {
+        axios({
+            url: '/api/who-i-am',
+            method: 'GET'
+        }).then((response) => {
+            if(response.status !== 200) {
+                throw new Error();
+            }
+
+            setIsAuthorized(true);
+
+        }).catch(() => {
+            window.location.href = '/';
+            setIsAuthorized(false);
+
+        }).finally(() => {
+            setIsLoading(false);
+        })
+    }
 
     return (
         <div>
-            <Router>
-                <Switch>
-                    <Route path={'/demo'}><Demo /></Route>
-                    <Route path={'/dashboard'}><Dashboard /></Route>
-                    <Route path={'/'}>
-                        { isAuthorized ? <Redirect to={'/dashboard'} /> : <Auth /> }
-                    </Route>
-                </Switch>
-            </Router>
+            {
+                isLoading ?
+                    <Loading type={'page'} />
+                    :
+                    <Router>
+                        <Switch>
+                            <Route path={'/demo'}><Demo /></Route>
+                            <Route path={'/dashboard'}><Dashboard /></Route>
+                            <Route path={'/'}>
+                                { isAuthorized ?
+                                    <Redirect to={'/dashboard'} /> :
+                                    <Auth />
+                                }
+                            </Route>
+                        </Switch>
+                    </Router>
+            }
         </div>
     )
 }
