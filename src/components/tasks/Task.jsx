25bcebe1275475c855512b992aa5dc8e290ca2d9
klavrenuk <klavrenuk@yandex.ru>
2022-03-30 10:06:55 +0300

    Task - change render

--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {Button, Col, Input, Label, Row} from "reactstrap";
 import {AiFillDelete, AiFillEdit} from "react-icons/ai";
 
 import './css/task.min.css';
 
 export default function Task(props) {
-    const task = props.task;
+    const [task, setTask] = useState(props.task, {});
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    const clickPressListener = (event) => {
+        if(!event.target.closest('.create_task')) {
+            console.log('close');
+            // props.toggleViewCreateTask(false);
+        }
+    }
+
+    const memoizedListener = useMemo(() => clickPressListener, []);
+
+    useEffect(() => {
+        window.addEventListener('click', memoizedListener);
+
+        return() => {
+            window.removeEventListener('click', memoizedListener);
+        };
+    }, [memoizedListener]);
+
+    const onKeyDown = (event) => {
+        if(event.code === 'Enter') {
+            props.toggleViewCreateTask(false);
+        }
+    }
+
+    const onChange = (event) => setTask({
+        ...task,
+        name: event.target.value
+    });
+
+    console.log(task.name);
 
     return (
         <li className={'task'}>
             <Row className={'flex flex--align_center'}>
                 <Col sm={8}>
-                    <Input id={task._id.toString()}
-                           type="checkbox"
+                    <Input className={'task-checkbox'}
+                           type={'checkbox'}
+                           disabled={isDisabled}
                     />
-                    <Label className={'task-label'}
-                           for={task._id.toString()}
-                    >{ task.name }</Label>
+                    <Input id={task._id.toString()}
+                           value={task.name}
+                           placeholder={'Please, enter name of task'}
+                           className={'task-text'}
+                           type="text"
+                           disabled={true}
+                           onKeyDown={(event) => onKeyDown(event)}
+                           onChange={(event) => onChange(event)}
+                    />
                 </Col>
                 <Col sm={4} className={'text-right task-controller'}>
                     <Button color={'icon'}>
