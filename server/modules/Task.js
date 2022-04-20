1dc166e72c57343789a0b90c8de57d1f87acafb8
klavrenuk <klavrenuk@yandex.ru>
2022-04-20 09:16:47 +0300

    remove custom throw

--- server/modules/Task.js
+++ server/modules/Task.js
@@ -84,8 +84,6 @@ const Task = {
                 return false;
             }
 
-            throw Error('Custom error');
-
             ModelTask.findByIdAndDelete({_id: task._id}, (err) => {
                 if(err) {
                     reject(err);
