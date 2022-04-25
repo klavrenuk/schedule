3f366025e9a616358e00110529b4100f531a56bd
klavrenuk <klavrenuk@yandex.ru>
2022-04-25 13:59:45 +0300

    Hour - render(step1)

--- src/components/schedule/Hour.jsx
+++ src/components/schedule/Hour.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import './css/hour.min.css';
+
 export default function Hour(props) {
+    const renderHourToString = hour => {
+        if(hour < 10) {
+            return `0${hour}:00`;
+        } else {
+            return `${hour}:00`;
+        }
+    }
+
     return (
         <div className={'hour'}>
-            <div className={'hour-left_side'}>{props.hour}</div>
+            <div className={'hour-left_side'}>
+                <span className={'left_side-hour'}>
+                    {renderHourToString(props.hour)}
+                </span>
+            </div>
+
             <div className={'hour-line'}></div>
         </div>
     )
--- src/components/schedule/time.js
+++
@@ -1,9 +0,0 @@
-export default class Time {
-    getListHours() {
-        let hours = [];
-
-        for(let i = 0; i < 24; i++) {
-            list.push()
-        }
-    }
-}
