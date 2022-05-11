86d4f307c20bca83b11be57a1d3afc73844dec3b
klavrenuk <klavrenuk@yandex.ru>
2022-05-11 19:37:59 +0300

    EventFormItem - change logic saving value

--- src/components/event/EventFormItem.jsx
+++ src/components/event/EventFormItem.jsx
@@ -2,17 +2,11 @@ import React, {useState} from 'react';
 import {FormGroup, Input, Label, Button, Row, Col} from "reactstrap";
 import DatePicker from "react-multi-date-picker"
 import TimePicker from "react-multi-date-picker/plugins/time_picker";
-import {useDispatch} from 'react-redux';
 
 import './css/event_form_item.min.css';
 
 export default function EventFormItem(props) {
-    const dispatch = useDispatch();
     const [isShowDescription, setIsShowDescription] = useState(false);
-    const [date, setDate] = useState({
-        start: new Date().setHours(0,0,0,0),
-        end: new Date().setHours(23,59,59,59)
-    });
     const data = props.data;
 
     function renderWithFromGroup(content) {
@@ -33,31 +27,9 @@ export default function EventFormItem(props) {
         )
     }
 
-    const onChangeItem = (event) => {
-        dispatch({
-            type: 'event',
-            prop: event.target.name,
-            value: event.target.value
-        });
-    }
+    const onChangeItem = (event) => data.value = event.target.value;
 
-    const onChangeDate = (value, type) => {
-        setDate({
-            ...date,
-            [type]: value
-        })
-        setStartDate(date);
-
-        console.log('onChangeDAte', date)
-
-        return false;
-
-        // dispatch({
-        //     type: 'event',
-        //     prop: data.name,
-        //     value: new Date(date).getTime()
-        // });
-    }
+    const onChangeDate = (value, type) => data[type] = value.unix * 1000;
 
     const toggleViewDescription = () => setIsShowDescription(true);
 
@@ -100,9 +72,8 @@ export default function EventFormItem(props) {
                     <Row className={'flex flex--align_center'}>
                         <Col sm={5}>
                             <DatePicker
-                                value={date.start}
                                 format="dd/MM/YYYY HH:mm:ss"
-                                onChange={(date) => onChangeDate(dateStart, 'start')}
+                                onChange={(value) => onChangeDate(value, 'start')}
                                 plugins={[
                                     <TimePicker position="bottom" />
                                 ]}
@@ -111,20 +82,15 @@ export default function EventFormItem(props) {
                         <Col sm={2} className={'text-center'}>-</Col>
                         <Col sm={5}>
                             <DatePicker
-                                value={date.end}
                                 format="dd/MM/YYYY HH:mm:ss"
-                                onChange={(date) => onChangeDate(dateEnd, 'end')}
+                                onChange={(value) => onChangeDate(value, 'end')}
                                 plugins={[
                                     <TimePicker position="bottom" />
                                 ]}
                             />
                         </Col>
-
                     </Row>
-
-
                 </div>
-
             )
 
         default:
